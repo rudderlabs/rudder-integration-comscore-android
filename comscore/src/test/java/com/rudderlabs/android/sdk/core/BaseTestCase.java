3b7f5dd572e94651e7a47702d01b352730154545
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2023-10-17 14:41:01 +0530

    chore: remove dummy test case

--- comscore/src/test/java/com/rudderlabs/android/sdk/core/BaseTestCase.java
+++ comscore/src/test/java/com/rudderlabs/android/sdk/core/BaseTestCase.java
@@ -1,11 +1,11 @@
-package com.rudderlabs.android.sdk.core;
-
-
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
-
-@RunWith(RobolectricTestRunner.class)
-@Config(sdk = 28)
-public class BaseTestCase {
-}
+//package com.rudderlabs.android.sdk.core;
+//
+//
+//import org.junit.runner.RunWith;
+//import org.robolectric.RobolectricTestRunner;
+//import org.robolectric.annotation.Config;
+//
+//@RunWith(RobolectricTestRunner.class)
+//@Config(sdk = 28)
+//public class BaseTestCase {
+//}
